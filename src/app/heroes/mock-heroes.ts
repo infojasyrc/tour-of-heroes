9e205ddf54b4d5ba0e3d2b3c60098f18bef773fb
Jose Sal y Rosas <infojasyrc@gmail.com>
2018-05-20 21:05:43 -0500

    Displaying a List

---
+++ src/app/heroes/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Iron Man' },
+  { id: 12, name: 'Doctor Strange' },
+  { id: 13, name: 'Captain America' },
+  { id: 14, name: 'Hulk' },
+  { id: 15, name: 'Thor' },
+  { id: 16, name: 'Black Panther' },
+  { id: 17, name: 'Scarlet Witch' },
+  { id: 18, name: 'Ant-Man' },
+  { id: 19, name: 'Spiderman' },
+  { id: 20, name: 'Winter Solder' }
+];
