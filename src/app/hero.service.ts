765b8c5e83f5f6325f092a18629f541245a52d90
Jose Sal y Rosas <infojasyrc@gmail.com>
2018-06-06 19:50:19 -0500

    Added routing features

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+
 import { Hero } from './_models/hero';
 import { HEROES } from './_models/mock-heroes';
 import { MessageService } from './message.service';
@@ -15,5 +16,11 @@ export class HeroService {
     // TODO: send the message _after_ fetching the heroes
     this.messageService.add('Hero Service: fetched heroes');
     return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // TODO: send a message after fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
   }
 }
