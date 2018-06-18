02a51ba2c5954e8892d20903f81a1f1c768116f0
Jose Sal y Rosas <infojasyrc@gmail.com>
2018-06-17 23:49:26 -0500

    Improved unit tests
    - Modified heroes.component.spec to run all unit tests.
    - Modified hero.service.spec.ts to run all unit tests.

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -4,17 +4,25 @@ import { APP_BASE_HREF } from '@angular/common';
 
 import { AppRoutingModule } from './../app-routing.module';
 import { FormsModule } from '@angular/forms';
+import { of } from 'rxjs';
+
+import { HeroService } from './../hero.service';
 
 import { DashboardComponent } from './../dashboard/dashboard.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './../hero-detail/hero-detail.component';
 import { HeroSearchComponent } from './../hero-search/hero-search.component';
 
+
 describe('HeroesComponent', () => {
   let component: HeroesComponent;
   let fixture: ComponentFixture<HeroesComponent>;
 
   beforeEach(async(() => {
+    const heroService = jasmine.createSpyObj('HeroService', ['getHeroes']);
+
+    heroService.getHeroes.and.returnValue(of([]));
+
     TestBed.configureTestingModule({
       declarations: [
         DashboardComponent,
@@ -24,6 +32,7 @@ describe('HeroesComponent', () => {
       ],
       imports: [ AppRoutingModule, FormsModule ],
       providers: [
+        { provide: HeroService, useValue: heroService },
         { provide: APP_BASE_HREF, useValue: '/' }
       ]
     })
